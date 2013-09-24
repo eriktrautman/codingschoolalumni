f68cc849a9d84c764d28d33bc47ee9f81e83eb5d
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-09-24 15:45:01 -0700

    initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codingschoolalumni::Application.config.secret_key_base = '78a9c1a0ad8aa026e3f4a281ae6fdc0b0f2d697793a640723410404babccfc872602d84f85363ff6ed5821192b166f19548b156650147ea8496f02d7695030d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codingschoolalumni::Application.config.session_store :cookie_store, key: '_codingschoolalumni_session'
