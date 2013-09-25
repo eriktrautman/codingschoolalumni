97ca6b3fa24f2b4d979c6f660509f1282a0170a6
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-09-24 20:52:38 -0700

    include the cohort city in the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @schools_list = School.all
-    @cohorts_list = Cohort.includes(:city).all # add an eager load of cities here
+    @cohorts_list = Cohort.includes(:city).map do |c|
+      {
+        :id => c.id, 
+        :city => c.city.abbrev,
+        :end_date => c.end_date,
+        :start_date => c.start_date,
+        :school_id => c.school_id
+      }
+    end
   end
 
 end
