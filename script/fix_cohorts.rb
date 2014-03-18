ec417a4b9ac83ddcb4bc3455a7c8dea00f63b9df
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-03-18 13:38:29 -0700

    fix username bug

--- script/fix_cohorts.rb
+++ script/fix_cohorts.rb
@@ -14,7 +14,7 @@ file_contents.split("\n").each_with_index do |l,i|
   arr = l.split(",")
   # puts arr.size
   u = User.where(:email => arr.first).first
-  puts "User: #{u.first_name}!"
+  puts "User: #{u.fname} #{u.lname}!"
   if Cohort.where(:id => u.cohort_id).empty?
     puts "Can't find the cohort!"
     city = City.where(:name => arr[5]).first
@@ -26,6 +26,6 @@ file_contents.split("\n").each_with_index do |l,i|
     cohort = Cohort.where(:city_id => city.id, :school_id => school.id, :start_date => arr[6])
     
     raise "couldn't find the cohort!" if cohort.nil?
-    puts "#{u.first_name} should be in cohort #{cohort.id}!"
+    puts "#{u.fname} #{u.lname} should be in cohort #{cohort.id}!"
   end
 end
