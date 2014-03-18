1fb0c3fff5dc9a872fb17f7671c217fcd0d912e9
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-03-18 13:35:39 -0700

    update script to load from S3 file

--- script/fix_cohorts.rb
+++ script/fix_cohorts.rb
@@ -1,10 +1,14 @@
 # Implemented to fix the stupidity of using the db:seeds file,
 # which blows away all data, in production and therefore having
 # member cohorts be all screwed up.
+require 'open-uri'
 
 puts "test"
 
-File.open(ENV["USER_EXPORT_20140318"]).each_with_index do |l,i|
+file_contents = open("https://codingschoolalumni.s3.amazonaws.com/members_export_5ebad133f8_20140318.csv") {|f| f.read}
+# puts file_contents
+
+file_contents.split("\n").each_with_index do |l,i|
   next if i < 1
   # puts l
   arr = l.split(",")
@@ -17,8 +21,10 @@ File.open(ENV["USER_EXPORT_20140318"]).each_with_index do |l,i|
     puts "City: #{city.name}!"
     school = School.where(:name => arr[4]).first
     puts "School: #{school.name}!"
+
     raise "something screwed up" if u.nil? || city.nil? || school.nil?
     cohort = Cohort.where(:city_id => city.id, :school_id => school.id, :start_date => arr[6])
+    
     raise "couldn't find the cohort!" if cohort.nil?
     puts "#{u.first_name} should be in cohort #{cohort.id}!"
   end
